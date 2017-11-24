ff3f75070bb2354f1de86ffc3dcc3f67ad3a05bd
matteoveronesi <32384060+matteoveronesi@users.noreply.github.com>
2017-11-24 17:47:39 +0100

    Primo commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function header_showcat()
+{
+	document.getElementById('categories').style.display = 'inline';
+	document.getElementById('about').style.display = 'none';
+}
+
+function header_showinfo()
+{
+	document.getElementById('categories').style.display = 'none';
+	document.getElementById('about').style.display = 'inline';
+}
+
+function header_changelang()
+{
+	var lang = document.getElementById('language');
+	if (lang.style.display == 'block') lang.style.display = 'none';
+	else lang.style.display = 'block';
+}
